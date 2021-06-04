dc584ddd316ff2d18a472f4344f4703a80593a38
Tanmay <chowdhurytanmay642@gmail.com>
2021-06-05 00:49:31 +0530

    YoursTanmay Fitness

--- Asset/js/Template/nav.js
+++ Asset/js/Template/nav.js
@@ -34,6 +34,7 @@ $(function () {
     bar += '                </a>';
     bar += '                <div class="dropdown-menu bg-secondary" aria-labelledby="navbarDropdownMenuLink">';
     bar += '                     <a class="dropdown-item" href="https://pinup.42web.io" target="_blank">Pinup(Social Networking)</a>';
+    bar += '                     <a class="dropdown-item" href="https://yourstanmay.github.io/fitness" target="_blank">YoursTanmay Fitness</a>';
     bar += '                     <a class="dropdown-item" href="https://yourtanmay.000webhostapp.com/" target="_blank">YoursTanmay Banking <span class="badge badge-danger">&nbsp;Confidential🔒</span></a>';
     bar += '                     <a class="dropdown-item" href="https://yourstanmay.000webhostapp.com/AdminPanel" target="_blank">Admin Panel <span class="badge badge-danger">&nbsp;🔒</span></a>';
     bar += '                </div>';
