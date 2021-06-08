7fc58d520c8db11fa79e6e059fa4a0a04c82fc43
Tanmay <chowdhurytanmay642@gmail.com>
2021-06-08 22:22:52 +0530

    Url

--- Asset/js/Template/nav.js
+++ Asset/js/Template/nav.js
@@ -41,7 +41,7 @@ $(function () {
     bar += '            </div>';
     bar += '            <a class="nav-item nav-link" href="/Content/video.html">🎥Videos</a>';
     bar += '            <a class="nav-item nav-link" href="/Content/store.html">🏬Play Store</a>';
-    bar += '            <a class="nav-item nav-link" href="https://yourstanmay.epizy.com" title="Watch Movie & Video Song" target="_blank">🎬Cinematic </a>';
+    bar += '            <a class="nav-item nav-link" href="https://yourstanmay.epizy.com/Cinematic.php" title="Watch Movie & Video Song" target="_blank">🎬Cinematic </a>';
     bar += '            <a class="nav-item nav-link" href="https://yourstanmay.000webhostapp.com/Notebook/" target="_blank">📓NoteBook</a>'; 
     bar += '            <a class="nav-item nav-link" href="https://www.onlinegdb.com/" target="_blank">👨‍💻Compiler</a>';
     bar += '            <a class="nav-item nav-link" href="https://yourstanmay.000webhostapp.com/AdminPanel/contactMe.php">📧Contact Me</a>';
